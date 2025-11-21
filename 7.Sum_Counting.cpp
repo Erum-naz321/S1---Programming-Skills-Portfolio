6dbf523d45fc1b19169d222ad5f9bf86cf6eef82
Erum-naz321 <erum.naz24@bathspa.ac.uk>
2025-11-21 23:49:58 +0500

    Update and rename Sum_Counting.cpp to 7.Sum_Counting.cpp

---
+++ 7.Sum_Counting.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Count 0 → 50
+    for (int i = 0; i <= 50; i++)
+        cout << i << " ";
+    cout << endl;
+
+    // Count 50 → 0
+    for (int i = 50; i >= 0; i--)
+        cout << i << " ";
+    cout << endl;
+
+    // Count 30 → 50
+    for (int i = 30; i <= 50; i++)
+        cout << i << " ";
+    cout << endl;
+
+    // Count 50 → 10 (step -2)
+    for (int i = 50; i >= 10; i -= 2)
+        cout << i << " ";
+    cout << endl;
+
+    // Count 100 → 200 (step +5)
+    for (int i = 100; i <= 200; i += 5)
+        cout << i << " ";
+}
