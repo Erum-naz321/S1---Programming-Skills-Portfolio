cc727a38e7b2c4a9b343fa8c25a8f943a51026f6
Erum-naz321 <erum.naz24@bathspa.ac.uk>
2025-11-21 23:41:32 +0500

    Update and rename simple_sums.cpp to 2.simple_sums.cpp

---
+++ 2.simple_sums.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare integer variables
+    int a = 8;   // First number
+    int b = 10;  // Second number
+
+    // Add them together
+    int sum = a + b;
+
+    // Print the result
+    cout << sum;
+
+    return 0;
+}
