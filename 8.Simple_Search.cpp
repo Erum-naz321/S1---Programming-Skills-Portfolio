ac284b026540bfacce9da7b318ecf519d39fade9
Erum-naz321 <erum.naz24@bathspa.ac.uk>
2025-11-21 23:50:16 +0500

    Update and rename Simple_Search.cpp to 8.Simple_Search.cpp

--- 8.Simple_Search.cpp
+++ 8.Simple_Search.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 int main() {
+    // Array of names
     string names[] = {"Jake", "Zac", "Ian", "Ron", "Sam", "Dave"};
-    string search;
 
-    cout << "Enter a name to search: ";
-    cin >> search;
+    // Name to search
+    string search = "Sam";
 
+    // Loop through array
     for (int i = 0; i < 6; i++) {
-        if (names[i] == search) {
+        if (names[i] == search) {  // Compare each name
             cout << search << " found in the list.";
             return 0;
         }
