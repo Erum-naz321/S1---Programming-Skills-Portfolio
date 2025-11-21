3c07f2bfc9e4276708fe9115653dcd5785a6ece6
Erum-naz321 <erum.naz24@bathspa.ac.uk>
2025-11-21 23:52:51 +0500

    Update and rename Is_Even.cpp to 10.Is_Even.cpp

--- 10.Is_Even.cpp
+++ 10.Is_Even.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
 
+// Function that checks if a number is even or odd
 string checkEven(int n) {
+
+    // If number divisible by 2
     if (n % 2 == 0)
         return "The provided number is even";
     else
@@ -10,8 +13,11 @@ string checkEven(int n) {
 
 int main() {
     int num;
+
+    // Ask the user for a number
     cout << "Enter a number: ";
     cin >> num;
 
+    // Print the returned message
     cout << checkEven(num);
 }
