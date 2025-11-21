6028d40c66fc97e4f381bac695470e15e35dc8ba
Erum-naz321 <erum.naz24@bathspa.ac.uk>
2025-11-21 23:49:31 +0500

    Update and rename Brute_Force.cpp to 6.Brute_Force.cpp

--- 6.Brute_Force.cpp
+++ 6.Brute_Force.cpp
@@ -2,22 +2,27 @@
 using namespace std;
 
 int main() {
-    int password = 12345;
+    int password = 12345; // The correct password
     int attempt;
-    int tries = 5;
+    int tries = 5;        // User has 5 attempts
 
+    // Loop until no attempts remain
     while (tries > 0) {
+
         cout << "Enter password: ";
         cin >> attempt;
 
+        // Check if correct
         if (attempt == password) {
             cout << "Welcome to the Secure Area";
-            return 0;
+            return 0; // Exit program
         }
 
+        // If wrong, reduce attempts
         tries--;
         cout << "Incorrect. Attempts left: " << tries << endl;
     }
 
+    // If all attempts are used
     cout << "Too many attempts. Authorities alerted!";
 }
