8b8d46f8c526c533c548f9d8a7493288f901c78c
Erum-naz321 <erum.naz24@bathspa.ac.uk>
2025-11-21 23:51:22 +0500

    Update and rename Hello.cpp to 9.Hello.cpp

--- 9.Hello.cpp
+++ 9.Hello.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
+// Define a function named hello
 void hello(){
+    // Print Hello
     cout << "Hello" << endl;
 }
 
 int main(){
+
+    // Call the function
     hello();
+
     return 0;
 }
