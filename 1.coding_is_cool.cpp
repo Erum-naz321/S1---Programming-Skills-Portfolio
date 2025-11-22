d72a6bb93d9f8ec04c2b1e6f769d4b5ad60147b6
Erum-naz321 <erum.naz24@bathspa.ac.uk>
2025-11-22 19:15:53 +0000

    Update 1.coding_is_cool.cpp

--- 1.coding_is_cool.cpp
+++ 1.coding_is_cool.cpp
@@ -1,13 +1,16 @@
 // Exercise 1: Coding is Cool
 // This program prints a simple message to the console.
 
-#include <iostream>
+#include <iostream>// declares objects that control reading input and output
+#include <string> // including strig data type
+using namespace std; // declare a standard namespace
 #include <string>
 using namespace std;
-
+// Fill in the blanks in the code below so the program outputs "Coding is Cool" to the console.
 int main() {
     cout << "Coding is Cool" << endl;   // Output message to the console
     return ();   // End of program
 }
+
 
 
