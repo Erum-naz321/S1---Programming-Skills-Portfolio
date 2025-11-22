4c48740e3cfb47ae66e6e8b3fd0b991e946a6b9d
Erum-naz321 <erum.naz24@bathspa.ac.uk>
2025-11-22 00:22:10 +0000

    Update 1.coding_is_cool.cpp

--- 1.coding_is_cool.cpp
+++ 1.coding_is_cool.cpp
@@ -1,3 +1,6 @@
+// Exercise 1: Coding is Cool
+// This program prints a simple message to the console.
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -6,4 +9,5 @@ int main() {
     cout << "Coding is Cool" << endl;   // Output message to the console
     return ();   // End of program
 }
+
 
